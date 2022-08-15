3c11a99fde5ce44f8cda46b1489f710145887d49
jayocaine <66975498+jayocaine@users.noreply.github.com>
2022-08-14 23:07:15 -0700

    Second Commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var Xaxis = Input.GetAxis("Horizontal");
+        var yAxis = Input.GetAxis("Vertical");
+
+        transform.Translate(new Vector3(Xaxis * 0.05f,yAxis * 0.05f, 0));
+    }
+}
---
+++ Assets/TweenPlayer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class TweenPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow)|| Input.GetKeyDown(KeyCode.D))
+        {
+            transform.DORotate(new Vector3(0,0,-30), 0.1f, RotateMode.Fast);
+            transform.DOScaleX(0.8f, 0.1f);
+            transform.DOScaleY(1.2f, 0.1f);
+        }       
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+        {
+            transform.DORotate(new Vector3(0, 0, 30), 0.1f, RotateMode.Fast);
+            transform.DOScaleX(0.8f, 0.1f);
+            transform.DOScaleY(1.2f, 0.1f);
+        }
+        if (Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.D))
+        {
+            transform.DORotate(new Vector3(0, 0, 0), 0.1f, RotateMode.Fast).SetEase(Ease.OutBounce);
+            transform.DOScaleX(1, 0.1f);
+            transform.DOScaleY(1, 0.1f);
+
+        }
+        if (Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.A))
+        {
+            transform.DORotate(new Vector3(0, 0, 0), 0.1f, RotateMode.Fast).SetEase(Ease.OutBounce);
+            transform.DOScaleX(1, 0.1f);
+            transform.DOScaleY(1, 0.1f);
+        }
+    }
+}
